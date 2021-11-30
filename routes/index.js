7fd53198f3ee89828121d58b60a55f647ab406ca
Jason Leatiota <66217743+jasole1983@users.noreply.github.com>
2021-11-29 22:17:17 -0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,7 @@ if (process.env.NODE_ENV === 'production') {
     );
   });
 
-  router.use(express.static(path.join(__dirname, "react-ui/build")));
+  router.use(express.static(path.join(__dirname, "build")));
 
   router.get(/^(?!\/?api).*/, (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
