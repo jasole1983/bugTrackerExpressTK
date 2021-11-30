5e121ba257ed2c665ed5bcb404133573ebca0f77
Jason Leatiota <jasole2007@yahoo.com>
2021-11-30 01:37:53 -0800

    touched up home page

--- react-ui/src/features/Home/Home.js
+++ react-ui/src/features/Home/Home.js
@@ -2,13 +2,13 @@ import React from 'react'
 import './Home.css'
 
 export default function Home() {
-  const motd = "Here is the Message of the Day: Thanks for coming to check out my demo!  Feel free to poke around and try it out for yourself!"
+  const motd = `Here is the Message of the Day: Thanks for coming to check out my demo!  Feel free to poke around and try it out for yourself!`
   const footer = "Sincerely, jason.leatiota@gmail.com"
   const getTime = Date().toLocaleString().slice(0,25)
   
     return (
-        <div className="page-container home">
-          <div className="motd-card" id="motd">
+        <div className="page-cont home">
+          <div className="motd-card-cont" id="motd">
             <div className="motd-card hdr">
               {getTime}
             </div>    
